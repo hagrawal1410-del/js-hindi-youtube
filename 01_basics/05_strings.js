f22a3541801363315ebd1af322fa8f0312eed4a5
Himanshi Agrawal <hagrawal342@gmail.com>
2026-03-02 09:55:38 +0000

    string container

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,16 @@
+//string is object here.
+const name = "himanshi"
+const repocount = 50
+
+//console.log(name + repocount + "Value"); // nowdays noone use this 
+//console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+
+// string declare 
+const gameName  = new String("himanshi") // 0th index are present so key value pair.
+console.log(gameName[0]);
+console.log(gameName.__proto__);       //access prototype
+
+
+
+console.log(gameName.length);    //access length
+console.log(gameName.toUpperCase);    
--- 01_basics/datatypes summary.js
+++ 01_basics/datatypes summary.js
@@ -2,7 +2,7 @@
 // 7 types : string, Number, Boolean, null , undefined, symbol(to make unique value) , BigInt(scientic value, very big value)
 
 // js is statically type or dynamic type. in js we never define variable type.
-const score:number = 100 // typescript
+const score: number = 100 // typescript
 //no special type of decimal define here decimal comes under number.
 
 const isLoggedIn = false //bool type
@@ -19,7 +19,7 @@ const anotherid = symbol('123')
 
 
  
-// 2. Non-primitive (call by reference) 
+// 2. Non-primitive (call by reference)   return type is object. datatype is function.
 // Array, objects , Functions 
 // master browser event and objects to master js.
 const heroes = ['shaktiman', "naagraj" ,"doga"]   // array
@@ -32,12 +32,41 @@ let myObj = {
 const myFunction = function(){
     console.log("Hello ! World");
 }
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
 console.log(typeof bigNumber );  // of null => object
 // undefined => undefined
 // boolean => boolean
 // Number => Number
 // string => string
+//function => object function
 // object  native does not implement=> object
 // object  native or host and does implement=> function
 
 //find datatype  use typeof
+
+
+
+
+// ***********Memory*********
+//2 types - stack(Primitive type), gets copy for variable declare 
+//  heap(Non primitive), get reference of original value
+
+
+let myYoutubename = "himanshiagrawal.com"   //primitive example
+let anotherName =  myYoutubename // gives copy.
+let anotherName =  myYoutubename // myyoutubename value remains same.
+anothername = "chaiaurcode"
+console.log(myYoutubename);
+console.log(anotherName);
+
+let userone= {                             // non-primitive
+    email:  "user@google.com",
+    upi:  "user@ybl"
+}
+
+let usertwo = userone
+usertwo.email = "hitesh@giigle.com"
+
+console.log(userone.email);
+console.log(usertwo.email);
