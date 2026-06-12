b221891e782c3a8e24fb47527250a9da47a8f339
Himanshi Agrawal <hagrawal342@gmail.com>
2026-06-12 16:45:07 +0000

    1.pro

--- 09_API/02_promises.js
+++ 09_API/02_promises.js
@@ -5,7 +5,10 @@
 // fulfilled: meaning that the operation was completed successfully.
 // rejected: meaning that the operation failed.
 
-
+// when promise is not directly linked to node js then we use library like Q or bluebird
+// create promise
+// new instance create by new which is Promise (object)
+// 2 parameters: resolve & reject
 
 const promiseOne = new Promise(function(resolve, reject){
     //Do an async task
@@ -15,9 +18,10 @@ const promiseOne = new Promise(function(resolve, reject){
         resolve()
     }, 1000)
 })
-
+// resolve connect setTimeout(resolve) and .then
+//.then is directly associate with resolve
 promiseOne.then(function(){
-    console.log("Promise consumed");
+    console.log("Promise 1 consumed");
 })
 
 new Promise(function(resolve, reject){
@@ -27,9 +31,9 @@ new Promise(function(resolve, reject){
     }, 1000)
 
 }).then(function(){
-    console.log("Async 2 resolved");
+    console.log("Async 2 consumed");
 })
-
+// data pass
 const promiseThree = new Promise(function(resolve, reject){
     setTimeout(function(){
         resolve({username: "Chai", email: "chai@example.com"})
@@ -50,3 +54,44 @@ const promiseFour = new Promise(function(resolve, reject){
         }
     }, 1000)
 })
+
+// how to avoid callback hell
+// consumed by .then if error occur then .catch
+// promiseFour.then((user) => {
+//     console.log(user);
+//     return user.username   // where to return??
+// })
+
+// extract data , chaining of .then
+
+ promiseFour
+ .then((user) => {
+    console.log(user);
+    return user.username
+})
+.then((username) => {
+    console.log(username);  // error show so write catch
+})
+.catch(function(error){    // give any name
+    console.log(error);
+}).finally(() => console.log("The promise is either resolved or rejected"))
+
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if (!error) {
+            resolve({username: "Javascript", password: "123"})
+        } else {
+            reject('ERROR: JS went wrong')
+        }
+    }, 1000)
+})
+
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
